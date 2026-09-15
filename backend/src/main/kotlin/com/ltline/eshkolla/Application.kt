72294d8e789bc71e01e8aede924142fe83ab9d38
LTLINEDocumentation <ltline2024@gmail.com>
2026-09-15 22:58:40 +0200

    Fix Ktor call logging import

--- backend/src/main/kotlin/com/ltline/eshkolla/Application.kt
+++ backend/src/main/kotlin/com/ltline/eshkolla/Application.kt
@@ -13,7 +13,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.plugins.CallLogging
+import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.http.content.staticFiles

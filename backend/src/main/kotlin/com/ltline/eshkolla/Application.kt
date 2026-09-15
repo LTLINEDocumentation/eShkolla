bb7065509ec782bb6c30415d98ba9e93f81b1af8
LTLINEDocumentation <ltline2024@gmail.com>
2026-09-15 22:26:55 +0200

    Fix production web static routing imports

--- backend/src/main/kotlin/com/ltline/eshkolla/Application.kt
+++ backend/src/main/kotlin/com/ltline/eshkolla/Application.kt
@@ -12,12 +12,13 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.plugins.CallLogging
+import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.http.content.staticFiles
 import io.ktor.server.response.respondFile
 import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 import java.io.File
 import kotlinx.serialization.json.Json
 

0388975f971e0ff72df0b0718162c6aec979074f
LTLINEDocumentation <ltline2024@gmail.com>
2026-09-15 20:03:32 +0200

    release(android): bump eShkolla test APK to version 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ltline.eshkolla"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
         buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:8080\"")
     }
 

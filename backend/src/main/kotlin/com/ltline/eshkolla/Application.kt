e64edc475ddc77e0ae744eb1bc1e60adfe036b6e
LTLINEDocumentation <ltline2024@gmail.com>
2026-09-17 21:23:23 +0200

    Aktivizo sinkronizimin e përdoruesve MESIMDHENES me Mësimdhënësit

--- backend/src/main/kotlin/com/ltline/eshkolla/Application.kt
+++ backend/src/main/kotlin/com/ltline/eshkolla/Application.kt
@@ -21,6 +21,7 @@ import com.ltline.eshkolla.db.SchoolOperationsMigration
 import com.ltline.eshkolla.db.TeacherAccountProvisioningMigration
 import com.ltline.eshkolla.db.TeacherScheduleCodeMigration
 import com.ltline.eshkolla.db.TimetableMigration
+import com.ltline.eshkolla.db.UserTeacherProvisioningMigration
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
@@ -44,6 +45,7 @@ fun Application.module(){
  Database.connection().use(TimetableMigration::run)
  Database.connection().use(TeacherScheduleCodeMigration::run)
  Database.connection().use(TeacherAccountProvisioningMigration::run)
+ Database.connection().use(UserTeacherProvisioningMigration::run)
  install(CallLogging)
  install(ContentNegotiation){json(Json{prettyPrint=true;ignoreUnknownKeys=true;encodeDefaults=true})}
  configureStatusPages()
